e12827bae8184320220c2ec086808f32e9eadf0c
pitassarii53-lab <pitassarii53@gmail.com>
2026-02-04 00:01:27 +0700

    Update admin password in page.js
    Changed admin password from 'admin123' to '130903'.

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const SB_URL = "https://wakwbmuanzglmawqzopi.supabase.co"
 const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indha3dibXVhbnpnbG1hd3F6b3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMDc5MjYsImV4cCI6MjA4NTY4MzkyNn0.oVcKaJY9-RNu4QSk32fi3h8Lb-mBm4FXFuEfwKFmLZo"
-const PASSWORD_ADMIN = "admin123"
+const PASSWORD_ADMIN = "130903"
 const supabase = createClient(SB_URL, SB_KEY)
 
 export default function Admin() {
