cc6ffb8be5d3bf5ac9410363c4bb007569cd9019
pitassarii53-lab <pitassarii53@gmail.com>
2026-02-04 18:09:54 +0700

    Update metadata title and description in layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,15 +2,18 @@ import './globals.css'
 import Script from 'next/script'
 
 export const metadata = {
-  title: 'StreamingKu - Nonton Gratis',
-  description: 'Tempat nonton video viral terlengkap',
+  title: 'StreamingKu',
+  description: 'Nonton gratis tanpa ribet',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
-        {/* Iklan Pop-under Adsterra */}
+        {/* JURUS BIAR GAMBAR DOODSTREAM TIDAK BLANK */}
+        <meta name="referrer" content="no-referrer" />
+        
+        {/* Iklan Adsterra kamu */}
         <Script 
           src="https://pl27333728.effectivegatecpm.com/e4/6a/bf/e46abf385099c2b5d894dbb1c522e30c.js" 
           strategy="afterInteractive" 
